831d12ab392ac08fea96dce3f54567b9269ba834
StephenGodard <43731620+StephenGodard@users.noreply.github.com>
2018-12-08 22:25:44 +0100

    Update interface_graphique.py

--- python/webTarget/interface_graphique.py
+++ python/webTarget/interface_graphique.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 #-*- coding:utf-8-*-
 
 def Add():
-    newWindow = Toplevel()
+    main.destroy()
+    newWindow = Tk()
 
-    newWindow.focus_set()
-    newWindow.geometry("300x250")
+
+    newWindow.geometry("600x250")
 
     newWindow.title("Ajouter")
     label=Label(newWindow,text="Ajouter une adresse mail:")
@@ -22,8 +23,10 @@ def Add():
     buttonPrint.place(x=75,y=105)
 
 
+
     print("adress mail incorrect !")
 def Ajouter():
+
     car = "@"
     com = ".com"
     fr = ".fr"
@@ -34,42 +37,59 @@ def Ajouter():
             Adress.write(Adresslist)
             Adress.write("\n")
             Adress.close()
+            
             print("Ajouter !")
+
+
 
         elif fr in Adresslist:
             Adress = open("listeAdress.csv", "a")
             Adress.write(Adresslist)
             Adress.write("\n")
             Adress.close()
+
             print("Ajouter !")
+
+
     print("adress mail incorrect !")
 def double():
-    with open("listeAdress.csv") as Adresslist:
+    lines = open('listeAdress.csv', 'r').readlines()
 
-        line=[]
-        cnt=0
-        line_str=Adresslist.readline()
-        line.append(line_str)
+    lines_set = set(lines)
 
-        while line:
+    out = open('listeAdress.csv', 'w')
 
-            cnt += 1
-            line_str = Adresslist.readline()
-            line.append(line_str)
-            print(line[cnt])
-            if line[cnt]==line[cnt-1]:
-                print(line[cnt])
-                print(line_str)
+    for line in lines_set:
+        out.write(line)
+
+def ouverture():
+
+    newWindow = Toplevel()
+
+    newWindow.focus_set()
+    newWindow.geometry("500x250")
+    newWindow.title("Ajouter")
+    importer = Button(newWindow, text="Ouvrir fichier", command=ouverture)
+    importer.grid(row=0, column=0)
+    texte = Text(newWindow, width=80, height=30)
+    texte.grid(row=1, column=0)
+
+    filepath = filedialog.askopenfilename(filetypes=[('Adresselist.csv', '.csv')])
+
+    with open("listeAdress.csv", 'r') as FILE:
+        content = FILE.read()
+
+    texte.insert("end", content)
+
 import tkinter
 from tkinter import *
-
-
+from tkinter import filedialog
 main=Tk()
 main.configure(width="680",height="550")
 Add=Button(main,text="Ajouter",command=Add)
 Add.place(x=150,y=75)
-#csv=Button(main,text="import csv",command=csv)
-#csv.place(x=300,y=75)
+Printcsv=Button(main,text="import csv",command=ouverture)
+Printcsv.place(x=300,y=75)
 double=Button(main,text="Dédoubloner",command=double)
 double.place(x=150,y=150)
 main.mainloop()
